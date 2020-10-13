f5b49bf053fea1e8e018cc31a452ee0fe5f6765e
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-13 09:57:22 +0530

    Day 2

---
+++ DAY2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DAY2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum, product;
+            
+           
+
+            Console.WriteLine("Enter your nums");
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = a + b;
+            product = a * b;
+
+            Console.WriteLine("Sum=" + sum);
+            Console.WriteLine("Product="+product);
+
+            Console.ReadLine();
+        }
+    }
+}
