f246363d6c402fcf07ed4b140c203d7c136cdf63
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-13 10:53:57 +0530

    Calculator

--- DAY2/Program.cs
+++ DAY2/Program.cs
@@ -6,20 +6,65 @@ namespace DAY2
     {
         static void Main(string[] args)
         {
-            int a, b, sum, product;
-            
-           
+            int a, b, ans;
 
-            Console.WriteLine("Enter your nums");
+            Console.WriteLine("\t\t\tCalculator");
+            Console.WriteLine("\t\t\t----------");
+            Console.WriteLine("(1)SUM       (2)Product");
+            Console.WriteLine("(3)Division       (4)Subtraction");
 
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your choice?");
 
-            sum = a + b;
-            product = a * b;
 
-            Console.WriteLine("Sum=" + sum);
-            Console.WriteLine("Product="+product);
+
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (c==1)
+            {
+                Console.WriteLine("Enter your nums");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+
+                ans = a + b;
+                Console.WriteLine("Sum=" +ans);
+            }
+            else if (c == 2)
+            {
+                Console.WriteLine("Enter your nums");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+
+                ans = a * b;
+                Console.WriteLine("Product=" + ans);
+            }
+            else if (c == 3)
+            {
+                Console.WriteLine("Enter your nums");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+
+                ans = a/ b;
+                Console.WriteLine("Division=" + ans);
+            }
+            else if (c == 4)
+            {
+                Console.WriteLine("Enter your nums");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+
+                ans = a - b;
+                Console.WriteLine("Sub=" + ans);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
+            }
+
+
+
+
+
+
+          
 
             Console.ReadLine();
         }
