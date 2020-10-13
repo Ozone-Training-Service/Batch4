8103d340541dcfc00dcbcb707b04f11e0337b7cf
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-13 09:24:23 +0530

    Add project files.

---
+++ Calc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+            string name="1";
+            //Console.WriteLine("Enter  your name");
+            // name = Console.ReadLine();
+            // Console.WriteLine(name);
+
+            Console.WriteLine("Enter your nums");
+
+            a= Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+            Console.WriteLine("Sum="+c);
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\tResume");
+            Console.WriteLine("\t\t\t\t__________");
+            Console.WriteLine("\t\t\tName:----------------");
+
+
+
+
+            Console.ReadKey();// Similar to getch();
+        }
+    }
+}
