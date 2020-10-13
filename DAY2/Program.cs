258cf380a9e2abdd58610f16b4228058edf597ca
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-13 11:57:30 +0530

    Object Creation

--- DAY2/Program.cs
+++ DAY2/Program.cs
@@ -6,67 +6,13 @@ namespace DAY2
     {
         static void Main(string[] args)
         {
-            int a, b, ans;
-
-            Console.WriteLine("\t\t\tCalculator");
-            Console.WriteLine("\t\t\t----------");
-            Console.WriteLine("(1)SUM       (2)Product");
-            Console.WriteLine("(3)Division       (4)Subtraction");
-
-            Console.WriteLine("Enter your choice?");
-
-
-
-            int c = Convert.ToInt32(Console.ReadLine());
-            if (c==1)
-            {
-                Console.WriteLine("Enter your nums");
-                a = Convert.ToInt32(Console.ReadLine());
-                b = Convert.ToInt32(Console.ReadLine());
-
-                ans = a + b;
-                Console.WriteLine("Sum=" +ans);
-            }
-            else if (c == 2)
-            {
-                Console.WriteLine("Enter your nums");
-                a = Convert.ToInt32(Console.ReadLine());
-                b = Convert.ToInt32(Console.ReadLine());
-
-                ans = a * b;
-                Console.WriteLine("Product=" + ans);
-            }
-            else if (c == 3)
-            {
-                Console.WriteLine("Enter your nums");
-                a = Convert.ToInt32(Console.ReadLine());
-                b = Convert.ToInt32(Console.ReadLine());
-
-                ans = a/ b;
-                Console.WriteLine("Division=" + ans);
-            }
-            else if (c == 4)
-            {
-                Console.WriteLine("Enter your nums");
-                a = Convert.ToInt32(Console.ReadLine());
-                b = Convert.ToInt32(Console.ReadLine());
-
-                ans = a - b;
-                Console.WriteLine("Sub=" + ans);
-            }
-            else
-            {
-                Console.WriteLine("Invalid choice");
-            }
-
-
-
-
-
-
+            Test obj = new Test();
+           
           
 
-            Console.ReadLine();
+            obj.DoSmething();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ DAY2/Test.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY2
+{
+    public class Test
+    {
+        
+
+        public void DoSmething()
+        {
+
+
+            //int a, b, ans;
+
+            //Console.WriteLine("\t\t\tCalculator");
+            //Console.WriteLine("\t\t\t----------");
+            //Console.WriteLine("(1)SUM       (2)Product");
+            //Console.WriteLine("(3)Division       (4)Subtraction");
+
+            //Console.WriteLine("Enter your choice?");
+
+
+
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //if (c == 1)
+            //{
+            //    Console.WriteLine("Enter your nums");
+            //    a = Convert.ToInt32(Console.ReadLine());
+            //    b = Convert.ToInt32(Console.ReadLine());
+
+            //    ans = a + b;
+            //    Console.WriteLine("Sum=" + ans);
+            //}
+            //else if (c == 2)
+            //{
+            //    Console.WriteLine("Enter your nums");
+            //    a = Convert.ToInt32(Console.ReadLine());
+            //    b = Convert.ToInt32(Console.ReadLine());
+
+            //    ans = a * b;
+            //    Console.WriteLine("Product=" + ans);
+            //}
+            //else if (c == 3)
+            //{
+            //    Console.WriteLine("Enter your nums");
+            //    a = Convert.ToInt32(Console.ReadLine());
+            //    b = Convert.ToInt32(Console.ReadLine());
+
+            //    ans = a / b;
+            //    Console.WriteLine("Division=" + ans);
+            //}
+            //else if (c == 4)
+            //{
+            //    Console.WriteLine("Enter your nums");
+            //    a = Convert.ToInt32(Console.ReadLine());
+            //    b = Convert.ToInt32(Console.ReadLine());
+
+            //    ans = a - b;
+            //    Console.WriteLine("Sub=" + ans);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid choice");
+            //}
+
+
+
+            int x = 10;
+            Console.WriteLine("Num="+x);
+            Console.WriteLine("Hello From Test class ");
+        }
+    }
+}
