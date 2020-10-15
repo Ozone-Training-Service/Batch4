6bbc267a947f7ca62a72b6e4b8b7deb950bceef7
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-15 09:54:56 +0530

    Getter and Setter

---
+++ Day4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Console.WriteLine("Enter name");
+            //s1.x = Console.ReadLine();
+          
+            s1.setName(Console.ReadLine());
+
+            Console.WriteLine("Your Name is");
+            
+            Console.WriteLine(s1.getName());
+           
+        }
+    }
+}
---
+++ Day4/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    public class Student
+    {
+
+        int age;
+        public string x;
+
+        private int rollNo;
+
+     
+        
+        private string name;
+
+        public void setName(string name)
+        {
+            this.name = name;
+
+        }
+
+        public string getName()
+        {
+            return  name;
+        }
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void setRollNo(int rollNo)  // setter 
+        {
+            this.rollNo = rollNo;
+
+        }
+
+        public int getRollNo()  //get
+        {
+            return rollNo;
+        }
+
+
+    }
+}
