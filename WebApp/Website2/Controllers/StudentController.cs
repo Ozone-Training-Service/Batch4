b7c7a40ff9dffb61f14a26b40a5c400a07ca7cef
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-21 09:58:24 +0530

    ViewBag

---
+++ WebApp/Website2/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            ViewBag.Name = "Vishnu kumar";
+
+
+            ViewBag.Email = "vishnu@gmail.com";
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+
+        public string Hello()
+        {
+            return "hello";
+        }
+    }
+}
